f853e13dac7f4bf1b57af786e1b88df76b934a6d
Hope College HPC <hpc@hope.edu>
2019-07-09 15:53:22 -0400

    Initial commit

---
+++ singularity.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script contains user environment variables for Singularity
+# Place in /etc/profile.d to be run at login
+
+export SIF_PATH=/home/hope-singularity/image-files
+export DEF_FILES=/home/hope-singularity/definition-files
